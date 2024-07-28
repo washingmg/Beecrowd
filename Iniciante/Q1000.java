d1a2ecfa137c54b2bef244bc4f88c5df313717e3
Washington Medeiros <wmedeirosm001@gmail.com>
2024-07-28 10:53:52 -0300

    1000 - Hello World!

---
+++ Iniciante/Q1000.java
@@ -0,0 +1,8 @@
+package Iniciante;
+
+// PROBLEMA: 1000 - Hello World!
+public class Q1000 { //main
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
