d24c30ab08de1788b8c691ecc3e9bfa1bc636340
Washington Medeiros <wmedeirosm001@gmail.com>
2024-07-29 07:58:32 -0300

    1003 - Soma Simples

--- Iniciante/Q1003.java
+++ Iniciante/Q1003.java
@@ -1,3 +1,17 @@
-public class Q1003 {
-    
+package Iniciante;
+
+// PROBLEMA: 1003 - Soma Simples
+
+import java.util.Scanner;
+
+public class Q1003 { //main
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n1 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+
+        System.out.printf("SOMA = %d", n1 + n2);
+        scanner.close();
+    }
 }
