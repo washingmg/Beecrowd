f175208b530fa550da685c4bbadee2ca0c3f71b4
Washington Medeiros <144560662+washingmg@users.noreply.github.com>
2024-07-29 08:07:23 -0300

    Q1005.java

--- Iniciante/Q1005.java
+++ Iniciante/Q1005.java
@@ -8,16 +8,12 @@ public class Q1005 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        // Recebendo os valores de A e B como entrada do usuário
         double A = scanner.nextDouble();
         double B = scanner.nextDouble();
         
-        // Calculando a média ponderada
         double media = (3.5 * A + 7.5 * B) / 11;
         
-        // Imprimindo a média formatada com 5 casas decimais
         System.out.printf("MEDIA = %.5f\n", media);
-        
         scanner.close();
     }
 }
