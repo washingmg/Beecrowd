5e8efb88262507362925380ec7f33ffbab82ca14
Washington Medeiros <wmedeirosm001@gmail.com>
2024-07-30 08:23:48 -0300

    1018 - Cédulas

--- Iniciante/Q1018.java
+++ Iniciante/Q1018.java
@@ -8,14 +8,11 @@ public class Q1018 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Declaração das cédulas
         int[] cedulas = {100, 50, 20, 10, 5, 2, 1};
 
-        // Leitura do valor de entrada
         int dinheiro = scanner.nextInt();
         System.out.println(dinheiro);
 
-        // Processamento das cédulas
         for (int valor : cedulas) {
             int quantidade = dinheiro / valor;
             System.out.println(quantidade + " nota(s) de R$ " + valor + ",00");
