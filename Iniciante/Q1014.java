e39397773641b1f6ae00bec2b56fec5bc677cf27
Washington Medeiros <wmedeirosm001@gmail.com>
2024-07-30 08:24:20 -0300

    1014 - Consumo

--- Iniciante/Q1014.java
+++ Iniciante/Q1014.java
@@ -8,14 +8,10 @@ public class Q1014 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Leitura dos valores de entrada
         int x = scanner.nextInt();
         float y = scanner.nextFloat();
-
-        // Cálculo da eficiência de combustível
         float consumo = x / y;
 
-        // Impressão do resultado formatado
         System.out.printf("%.3f km/l%n", consumo);
         scanner.close();;
     }
